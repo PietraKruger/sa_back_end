de428354bea03891a2d572724c32b9b848ba1575
PIETRA KRUGER ANDRADE <pietra_andrade@estudante.sesisenai.org.br>
2026-03-30 15:53:42 -0300

    entrega parcial de hoje
    adicionei mais elementos ao mock de dados

--- src/services/filmes.service.js
+++ src/services/filmes.service.js
@@ -1,7 +1,14 @@
 const filmes = [
     {id: 1, nome:"Pecadores"},
     {id: 2, nome:"Homem Aranha Longe de Casa"},
-    {id: 3, nome:"Hamlet"}
+    {id: 3, nome:"Hamlet"},
+    {id: 4, nome:"Matrix"},
+    {id: 5, nome:"Premonição"},
+    {id: 6, nome:"A Bruxa de Blair"},
+    {id: 7, nome:"Mamma Mia"},
+    {id: 8, nome:"A Casa Monstro"},
+    {id: 9, nome:"A Convenção das Bruxas"},
+    {id: 10, nome:"Abracadabra"}
 ]
 
 class FilmesService {
